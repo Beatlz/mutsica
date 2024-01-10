d91b16b7803b08582671d9ad6db143a4c33eed03
Beatlz <adrian@basave.com>
2024-01-10 17:12:39 +0100

    feat: add endlessIndexOf()

--- src/utility/endlessArray.ts
+++ src/utility/endlessArray.ts
@@ -1,1 +1,3 @@
 export const endlessArray = <T>(array: readonly T[], start: number, end: number) => array[(start + end) % array.length]
+
+export const endlessIndexOf = <T>(array: readonly T[], start: number, end: number) => (start + end) % array.length
