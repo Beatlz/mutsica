29878e1a7ee5cf991e89de7ca572e9807c0644b9
Beatlz <adrian@basave.com>
2024-01-12 00:28:09 +0100

    fix: remove unused var

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,8 +1,7 @@
 import { Chord } from "./modules/Chord"
-import { Scale } from "./modules/Scale"
 import { romanToChord } from "./utility/romanToChord"
 
-const { degreeNumber, template } = romanToChord(`VII7`) as { degreeNumber: number; template: number[]; }
+const { template } = romanToChord(`VII7`) as { degreeNumber: number; template: number[]; }
 const notes = new Chord(`C`, template).notes
 
 console.log(notes)
